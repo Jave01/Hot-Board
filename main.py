cd1f51abebc0cb573dbf87c1c7fe5c0645d7e486
slin-j <55696587+slin-j@users.noreply.github.com>
2020-09-03 18:48:34 +0200

    bastle

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # file: main.py
 # project: Hot-Board
 # ******************************************
-from vcp_handler import vcp_reader
+import vcp_handler
 import event_handler
 
-s = vcp_reader()
+vcp = vcp_handler.vcp_reader()
+print(vcp.get_available_serial_ports())
+
 #s.print()
--- vcp_handler.py
+++ vcp_handler.py
@@ -3,17 +3,40 @@
 # project: Hot-Board
 # description: communication with the Hardware itself
 # ******************************************
+import sys
+import glob
+import serial
 import serial   # python -m pip install pyserial
 
 class vcp_reader():
-    vcp_baudrate = 9600     # standard baud
 
     def __init__(self):
-        serp = serial.Serial()
-        serp.baud = 9600
-        #serp.com = port_name
-        serp.open()
-        print(serp.name)
+        pass
 
-    def print_info(self):
-        print(serp.name)
+    def get_available_serial_ports(self):
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+        
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
