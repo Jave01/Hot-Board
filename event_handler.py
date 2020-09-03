3c0fe15b6dc656fbd3a74f7108f13cf107e84883
slin-j <55696587+slin-j@users.noreply.github.com>
2020-09-03 13:59:34 +0200

    files and headers

---
+++ event_handler.py
@@ -0,0 +1,5 @@
+# ******************************************
+# file: event_handler.py
+# project: Hot-Board
+# description: handles events, triggered by HW
+# ******************************************
---
+++ vcp_handler.py
@@ -0,0 +1,7 @@
+# ******************************************
+# file: vcp_handler.py
+# project: Hot-Board
+# description: communication with the Hardware itself
+# ******************************************
+
+
