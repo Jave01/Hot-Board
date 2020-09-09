495fc70296cdbea182d9d0e4d6cc508f9d688650
slin-j <55696587+slin-j@users.noreply.github.com>
2020-09-09 11:31:21 +0200

    187

--- main.py
+++ main.py
@@ -4,8 +4,35 @@
 # ******************************************
 import vcp_handler
 import event_handler
+import serial
+import json
+
+#ser = serial.Serial()
+#serial.Timeout = 0
+#ser.read()
 
 vcp = vcp_handler.vcp_reader()
 print(vcp.get_available_serial_ports())
+
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data['people'].append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+data['people'].append({
+    'name': 'Tim',
+    'website': 'apple.com',
+    'from': 'Alabama'
+})
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
 
 #s.print()
