d955674fa275034176e1ec946c1fac5643d3dffa
slin-j <55696587+slin-j@users.noreply.github.com>
2020-11-07 11:40:31 +0100

    test program for hardware

--- Software/pyApplication/main.py
+++ Software/pyApplication/main.py
@@ -1,11 +1,11 @@
 # ******************************************
 # file: main.py
-# project: Hot-Board
+# project: Hot-Board 
 # author: Nils Jäggi, David Jäggli
 # description: main routine
 # ******************************************
 import vcp_handler
-#import profile_handler
+import profile_handler
 import serial
 import json
 import sys
