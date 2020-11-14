8cca2d2805666e485f789319805e61cc0165cdc9
Y0URFR13ND <david.jaeggli@gmail.com>
2020-11-14 10:35:54 +0100

    Improved vcp handler. The function start_hot_board() automatically detects an STM fontroller and connects to it. Works on Win, Linux and (hopefully) MacOS
    Added key functions in profile_handler.py:
     - Execute File
     - Press Hotkey
     - Open URL
    
     At this point, the functions must be added manually in the xml file, but there are a few examples.
    
    Implemented special keys for hotkeys in xml file are:
    - 'Alt'
    - 'Ctrl'
    - 'Shift'
    - 'Win'
    - 'f4'
    - 'f7'
    - 'f9'
    
     Warning: Multiple profiles/users in one xml file does currently not work!

--- Software/pyApplication/profile_handler.py
+++ Software/pyApplication/profile_handler.py
@@ -1,31 +1,32 @@
 import os
 import xml.etree.ElementTree as ET
-
+from pynput.keyboard import Key, Controller
+import webbrowser
 
 
 class ProfileHandler():
-    def __init__(self, key_number: int):
+    def __init__(self, key_number: int = 12, path: str = 'profiles.xml'):
+        self.active_user = 'user1'
         self.key_number = key_number
-        self.hotkey_actions = ['start program', 'enter hotkey']
+        self.path = path
+        self.keyboard = Controller()
         # check if a profile file exists
         try:
-            with open('settings\\profiles.xml', 'r') as profiles:
-                self.tree = ET.parse('settings\\profiles.xml')
+            with open(self.path, 'r') as profiles:
+                self.tree = ET.parse(self.path)
 
         # If there is no valid profile file -> create one and fill it with the default parameters
         except FileNotFoundError:
-            setting_data = ET.Element('Settings')
-            users = ET.SubElement(setting_data, 'users')
-            user1 = ET.SubElement(users, 'user1')
-            keys = ET.SubElement(user1, 'keys')
+            settings_data = ET.Element('Settings')
+            users = ET.SubElement(settings_data, 'users')
+            user = ET.SubElement(users, 'user', name='user1')
             for i in range(self.key_number):
-                ET.SubElement(keys, 'key', name='s' + str(i), function='', additional_info='')
+                ET.SubElement(user, 'key', name='s' + str(i), function='', additional_info='')
 
-            self.tree = ET.ElementTree(setting_data)
-            self.tree.write('settings\\profiles.xml')
+            self.tree = ET.ElementTree(settings_data)
+            self.tree.write(self.path)
 
         self.root = self.tree.getroot()
-        print(self.root.tag)
         pass
 
     def export_profile(self):
@@ -34,41 +35,143 @@ class ProfileHandler():
     def import_profile(self):
         pass
 
-    def change_action(self):
-        """ Change the command which will be executed on press of any key"""
-        while True:
-            key = input('Enter valid Key Number: ')
-            # Check if key number is valid
-            if int(key) <= self.key_number and int(key) > 0:
-                break
-            else:
-                print('Key number must be between 1 and', self.key_number)
-
-        print('Enter new action, available actions are:')
-        for action in self.hotkey_actions:
-            print('- ' + action)
-
-        while True:
-            new_action = input('New Action: ')
-            if new_action == 'exit':
-                print('interrupted')
+    def change_user(self, new_user: str):
+        for user in self.root:
+            if user == new_user:
+                self.active_user = new_user
+                print('new user set')
                 return
+        print('No valid user found')
 
-            if new_action in self.hotkey_actions:
-                new_additional_info = input('Enter additional info (leave blank if not needed): ')
 
-                for k in self.root.iter('key'):
-                    if k.get('name') == 's' + str(int(key) - 1):
-                        k.set('function', new_action)
-                        k.set('additional_info', new_additional_info)
-                        self.tree.write('settings\\profiles.xml')
-                        print('new action set')
-                        break
-                else:
-                    print('something went wrong')
+
+
+    def change_action(self, key: str, new_action: str, action_info=''):
+        """
+        Change the command which will be executed on press of any key
+        Hand over some additional info if needed like a path.
+        :return: None
+        """
+        for u in self.root.iter(self.active_user):
+            print('Found user')
+            for k in u:
+                if k.get('name') == key:
+                    print('Found Key')
+                    k.set('function', new_action)
+                    k.set('additional_info', action_info)
+                    self.tree.write(self.path)
+                    print('wrote info')
+                    break
             else:
-                print('not valid, enter "exit" to interrupt the process')
+                continue
+            break
+
+    def execute_action(self, key):
+        """Searches the xml file for the given key and executes the action"""
+        for k in self.root.iter('key'):
+            if k.get('name') == key:
+                function = k.get('function')
+                info = k.get('additional_info')
+                if function != '':
+                    if function == 'start program':
+                        try:
+                            os.startfile(info)
+                        except FileNotFoundError:
+                            print('File on path:\n' + info + '\nnot Found')
+                    elif function == 'hotkey':
+                        keys_to_press = info.split(',')
+                        for k in keys_to_press:
+                            if len(k) == 1:
+                                self.keyboard.press(k)
+                            else:
+                                self.press_special_key(k)
+
+                        for k in keys_to_press:
+                            if len(k) == 1:
+                                self.keyboard.release(k)
+                            else:
+                                self.release_special_key(k)
+
+                    elif function == 'url':
+                        webbrowser.open(info)
+
+                    else:
+                        break
+        return
+
+    def press_special_key(self, key):
+        if key == 'Ctrl':
+            self.keyboard.press(Key.ctrl)
+        elif key == 'Shift':
+            self.keyboard.press(Key.shift)
+        elif key == 'Alt':
+            self.keyboard.press(Key.alt)
+        elif key == 'Win':
+            self.keyboard.press(Key.cmd)
+        elif key == 'f4':
+            self.keyboard.press(Key.f4)
+        elif key == 'f7':
+            self.keyboard.press(Key.f7)
+        elif key == 'f9':
+            self.keyboard.press(Key.f9)
+        return
+
+    def release_special_key(self, key):
+        if key == 'Ctrl':
+            self.keyboard.release(Key.ctrl)
+        elif key == 'Shift':
+            self.keyboard.release(Key.shift)
+        elif key == 'Alt':
+            self.keyboard.release(Key.alt)
+        elif key == 'Win':
+            self.keyboard.release(Key.cmd)
+        elif key == 'f4':
+            self.keyboard.release(Key.f4)
+        elif key == 'f7':
+            self.keyboard.release(Key.f7)
+        elif key == 'f9':
+            self.keyboard.release(Key.f9)
         return
 
 
+#region old crappy code
 
+#Note: The name of this region doesnt mean that my new code is better, I just dont like the one below
+
+# while True:
+#     key = input('Enter valid Key Number: ')
+#     # Check if key number is valid
+#     if int(key) <= self.key_number and int(key) > 0:
+#         break
+#     else:
+#         print('Key number must be between 1 and', self.key_number)
+#
+# print('Enter new action, available actions are:')
+# for action in self.hotkey_actions:
+#     print('- ' + action)
+#
+# while True:
+#     new_action = input('New Action: ')
+#     if new_action == 'exit':
+#         print('interrupted')
+#         return
+#
+#     if new_action in self.hotkey_actions:
+#         new_additional_info = input('Enter additional info (leave blank if not needed): ')
+#
+#         for k in self.root.iter('key'):
+#             if k.get('name') == 's' + str(int(key) - 1):
+#                 k.set('function', new_action)
+#                 k.set('additional_info', new_additional_info)
+#                 self.tree.write('settings\\profiles.xml')
+#                 print('new action set')
+#                 break
+#         else:
+#             print('something went wrong')
+#     else:
+#         print('not valid, enter "exit" to interrupt the process')
+# return
+
+
+# test = ProfileHandler()
+# test.change_action('user2', 's4', 'action nr.4', 'some info')
--- Software/pyApplication/vcp_handler.py
+++ Software/pyApplication/vcp_handler.py
@@ -7,6 +7,8 @@
 import glob
 import sys
 import serial  # python -m pip install pyserial
+from serial.tools import list_ports
+import glob
 
 
 class VirtualComPort():
@@ -14,27 +16,50 @@ class VirtualComPort():
         # protocoll constants
         self.numberOfDigits = number_of_digits
         self.switchIdentifier = switch_Identifier
-        # create vcp object
-        self.ser = serial.Serial()
 
-    def start_hot_board(self, com: str, baud: int = 115200) -> bool:
-        """ opens the comport
+        # Initialize the serial object
+        self.ser = serial.Serial(
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=0,
+            xonxoff=False,
+            rtscts=False,
+            dsrdtr=False,
+            writeTimeout=2
+        )
+
+    def start_hot_board(self, com: str = '', baud: int = 115200) -> bool:
+        """ searches for a connected board and tries to connect.
+            COM Port and baudrate can be changed manually if needed
 
             :returns:
                 flag if it worked or not
         """
-        self.ser.port = com
-        self.ser.baudrate = baud
-        self.ser.timeout = 0
-        # try opening vcp
+        if com == '':
+            ports = list_ports.comports()
+            for p in ports:
+                name = p.description
+                # searches for the STM Controller, it is named different on different OS
+                # And it depends on the OS language
+                if name.startswith('STM') or name.startswith('Seriell') or name.startswith('serial'):
+                    self.ser.port = p.device
+                    self.ser.baudrate = baud
+                    break
+        else:
+            self.ser.port = com
+            self.ser.baudrate = baud
+
         try:
             self.ser.open()
+            print('connection established')
             return True
         except serial.SerialException:
             return False
 
-    def check_switch_state(self) -> int:
-        # read serial port buffer
+    def check_switch_state(self) -> str:
+        """Reads the serial buffer and searches for a string which matches the
+        key format (for example s1) """
         serData = self.ser.read(len(self.switchIdentifier) + self.numberOfDigits).decode('ascii')
         # if data is valid
         if self.switchIdentifier in serData:
@@ -42,40 +67,15 @@ class VirtualComPort():
             retVal = 0
             for i in range(self.numberOfDigits):
                 retVal += int(serData[(i + 1) * (-1)]) * pow(10, i)
-            return retVal
+            return 's' + str(retVal)
         else:
-            return -1
+            return ''
 
-    def get_buffer_value(self):
-        """Returns serial buffer convertet to Ascii"""
+    def get_serial_ports(self) -> bool:
+        """ Returns a list of serial devices"""
+        return list_ports.comports()
+
+    def get_buffer_value(self) -> str:
+        """Returns serial buffer converted to Ascii"""
         serData = self.ser.read(256).decode('ascii')
         return serData
-
-    def get_available_serial_ports(self) -> list:
-        """ Lists serial port names
-
-            :raises EnvironmentError:
-                On unsupported or unknown platforms
-            :returns:
-                A list of the serial ports available on the system
-        """
-        if sys.platform.startswith('win'):
-            ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
-        else:
-            raise EnvironmentError('Unsupported platform')
-
-        result = []
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        return result
-
